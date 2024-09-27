90634b31e8bfdbe910bef495c8b24bc7eb8d4f6c
sumayaakter533 <sumaya55230@gmail.com>
2024-09-27 10:41:37 +0600

    challenge-1 added

---
+++ challenge_001.js
@@ -0,0 +1,18 @@
+/*
+Create a shopping list in the variable myList. The list should be a multi-dimensional array containing several sub-arrays.
+
+The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+["Chocolate Bar", 15]
+There should be at least 5 sub-arrays in the list.
+
+ *  1. myList should be an array.
+ *  2. The first elements in each of your sub-arrays should all be strings.
+ *  3. The second elements in each of your sub-arrays should all be numbers.
+ *  4. You should have at least 5 items in your list.
+
+*/
+
+let myList = [];
+myList = [["Chocolate Bar", 15], ["Ice cream", 30], ["Chips", 40], ["Mango Bar", 10], ["Biscuits", 50], ["Noodles", 20]];
+console.log(myList);
