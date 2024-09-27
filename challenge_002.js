68de766e67f7cb974ac6b251bb157f7e7e39fd61
sumayaakter533 <sumaya55230@gmail.com>
2024-09-27 10:45:10 +0600

    challenge-2 added

---
+++ challenge_002.js
@@ -0,0 +1,13 @@
+/*
+Create a function called reusableFunction which prints the string Hi World to the dev console. Call the function.
+
+*  1. reusableFunction should be a function.
+*  2. If reusableFunction is called, it should output the string Hi World to the console.
+*  3. You should call reusableFunction once it is defined.
+
+*/
+function reusableFunction () {
+    console.log('Hi World');
+}
+
+reusableFunction();
