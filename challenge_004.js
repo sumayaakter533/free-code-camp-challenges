9c8be9dc5281cee45a7d7a512320449578ef68a7
sumayaakter533 <sumaya55230@gmail.com>
2024-09-27 11:02:41 +0600

    challenge-4 added

---
+++ challenge_004.js
@@ -0,0 +1,16 @@
+/*
+Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.                 
+
+1. timesFive should be a function
+2. timesFive(5) should return 25
+3. timesFive(2) should return 10
+4. timesFive(0) should return 0
+
+*/
+
+function timesFive(num) {   
+    let mul = num*5
+    return mul
+}
+
+console.log(timesFive(5));
