ae3847f48102b173ac597122bcfed9c53c3e23fc
sumayaakter533 <sumaya55230@gmail.com>
2024-09-27 10:46:52 +0600

    challenge-3 added

---
+++ challenge_003.js
@@ -0,0 +1,18 @@
+/*
+Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+Call the function with two numbers as arguments.
+
+1. functionWithArgs should be a function.
+2. functionWithArgs(1,2) should output 3.
+3. functionWithArgs(7,9) should output 16.
+4. You should call functionWithArgs with two numbers after you define it.
+
+*/
+
+function functionWithArgs (param1, param2) {    
+    const result = param1 + param2;
+    console.log(result);
+}
+
+functionWithArgs(1, 2);
+functionWithArgs(7, 9);
